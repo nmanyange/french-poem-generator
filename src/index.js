6e463c8726030742726cdcba35bbff3e70ee034f
nmanyange <nmanyange30@gmail.com>
2024-10-05 16:42:31 +0200

    updated js file by adding api key

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter('#poem', {
-        strings: 'Hello World',
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
+}
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "b94f4fd195690f61bbe66t5d0211o3a0"
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let context = "You are a romantic poem expert and love r to write short poems. your mission is to generate 4 line poems. Make sure to follow user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
