d2a30ea90a806f5bf73e2bbd2256186d2d856a80
nmanyange <nmanyange30@gmail.com>
2024-10-06 07:27:06 +0200

    improved user experience

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generatePoem(event) {
 
     let poemElement = document.querySelector("#poem");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `Generating poem about ${instructionsInput.value}`;
+    poemElement.innerHTML = `<div class="generating">Generating poem about ${instructionsInput.value}</div>`;
 
     axios.get(apiUrl).then(displayPoem);
 }
